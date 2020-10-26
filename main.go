51da578f4b05af19536cae8ba81bc96c5e9704f6
Steve <MCTyler@users.noreply.github.com>
2020-10-26 13:06:01 -0500

    Update main.go

--- main.go
+++ main.go
@@ -19,25 +19,7 @@
 package main
 
 import "github.com/pierre-emmanuelJ/iptv-proxy/cmd"
-import (
-   "fmt"
-   "time"
-   "log"	
-)
 
 func main() {
 	cmd.Execute()
-   // manually set time zone
-   if tz := os.Getenv("TZ"); tz != "" {
-      var err error
-      time.Local, err = time.LoadLocation(tz)
-      if err != nil {
-         log.Printf("error loading location '%s': %v\n", tz, err)
-      }
-   }
-
-   // output current time zone
-   fmt.Print("Local time zone ")
-   fmt.Println(time.Now().Zone())
-   fmt.Println(time.Now().Format("2006-01-02T15:04:05.000 MST"))
 }
