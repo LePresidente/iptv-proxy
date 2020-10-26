7155ca5f3ece59eca0c3277d7da2d9616e42588c
Steve <MCTyler@users.noreply.github.com>
2020-10-26 12:42:12 -0500

    Update main.go
    Add TZ Variable

--- main.go
+++ main.go
@@ -22,4 +22,19 @@ import "github.com/pierre-emmanuelJ/iptv-proxy/cmd"
 
 func main() {
 	cmd.Execute()
+	
+func main() {
+   // manually set time zone
+   if tz := os.Getenv("TZ"); tz != "" {
+      var err error
+      time.Local, err = time.LoadLocation(tz)
+      if err != nil {
+         log.Printf("error loading location '%s': %v\n", tz, err)
+      }
+   }
+
+   // output current time zone
+   fmt.Print("Local time zone ")
+   fmt.Println(time.Now().Zone())
+   fmt.Println(time.Now().Format("2006-01-02T15:04:05.000 MST"))
 }
